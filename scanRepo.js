9e023f2e59eaa5549217bf094a3217de88a6ee87
Carlos Saito <carlos@saito.style>
2019-06-24 14:54:13 +0200

    Restore to previous head after traversing

--- scanRepo.js
+++ scanRepo.js
@@ -1,5 +1,7 @@
 const simpleGit = require('simple-git/promise')
 const scanDirectory = require('./scanDirectory')
+const fs = require('fs')
+const path = require('path')
 
 module.exports = async function scanRepo (repoPath, {from, to} = {}) {
   const git = simpleGit(repoPath)
@@ -12,6 +14,9 @@ module.exports = async function scanRepo (repoPath, {from, to} = {}) {
     console.log(`The repo [${repoPath}] is not clean.`)
   }
 
+  // Save the current "HEAD" to restore it after traversing
+  const headPath = path.resolve(repoPath, '.git/HEAD')
+  const headContent = fs.readFileSync(path.resolve(repoPath, '.git/HEAD'))
   const commits = (await git.log({from, to})).all
   const problems = []
 
@@ -21,4 +26,6 @@ module.exports = async function scanRepo (repoPath, {from, to} = {}) {
     scanDirectory(repoPath)
     console.groupEnd()
   }
+
+  fs.writeFileSync(headPath, headContent)
 }
