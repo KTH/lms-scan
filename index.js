89e04207fd11f8a596e66c2ff768ce1943ddf66a
Carlos Saito <carlos@saito.style>
2019-06-24 14:04:46 +0200

    Add a "history" option

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 #!/usr/bin/env node
 const scanDirectory = require('./scanDirectory')
+const scanRepo = require('./scanRepo')
 
+if (process.argv.length > 3 && process.argv[2] === 'history') {
+  const found = process.argv[3].match(/(\w+)\.\.(\w+)/)
+  if (found === null) {
+    console.log('Format not valid')
+    return
+  }
+  const [_, from, to] = found
+  scanRepo(process.cwd(), {from, to})
+  return
+}
 scanDirectory(process.cwd())
---
+++ scanRepo.js
@@ -0,0 +1,9 @@
+
+module.exports = function scanRepo (repoPath, {from, to} = {}) {
+  // Check if "repoPath" is actually a repository
+
+  // Loop through "from" to "to"
+  // - Checkout
+  // - Check directory
+  // Repeat
+}
