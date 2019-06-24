115e38b142ad607625ce4330c44a95fe35b31c77
Carlos Saito <carlos@saito.style>
2019-06-24 14:28:00 +0200

    Implementation of scanRepo

--- scanRepo.js
+++ scanRepo.js
@@ -1,9 +1,24 @@
+const simpleGit = require('simple-git/promise')
+const scanDirectory = require('./scanDirectory')
 
-module.exports = function scanRepo (repoPath, {from, to} = {}) {
-  // Check if "repoPath" is actually a repository
+module.exports = async function scanRepo (repoPath, {from, to} = {}) {
+  const git = simpleGit(repoPath)
 
-  // Loop through "from" to "to"
-  // - Checkout
-  // - Check directory
-  // Repeat
+  if (! (await git.checkIsRepo())) {
+    console.log(`Directory [${repoPath}] is not a git repository`)
+  }
+
+  if (! (await git.status()).isClean()) {
+    console.log(`The repo [${repoPath}] is not clean.`)
+  }
+
+  const commits = (await git.log({from, to})).all
+  const problems = []
+
+  for (const commit of commits) {
+    await git.checkout(commit.hash)
+    console.group(`Commit ${commit.hash}`)
+    scanDirectory(repoPath)
+    console.groupEnd()
+  }
 }
