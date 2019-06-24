6cbf0187f58588feb735097661a16a71ce8b9053
Carlos Saito <carlos@saito.style>
2019-06-24 16:41:12 +0200

    First test

--- test/scanDirectory.js
+++ test/scanDirectory.js
@@ -1,5 +1,11 @@
 const test = require('ava')
+const scanDirectory = require('../scanDirectory.js')
 
-test('foo', t => {
-  t.pass()
+test('foo', async t => {
+  const vulnerabilities = (await scanDirectory(__dirname + '/fake-data'))
+        .map(v => ({secrets: v.secrets}))
+
+  t.deepEqual(vulnerabilities, [{
+    secrets: ['8779~thisisatoken']
+  }])
 })
