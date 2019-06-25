61f87a98809d43558dcbf3af67bbb257f96dc674
Carlos Saito <carlos@saito.style>
2019-06-25 13:07:19 +0200

    Rewrite scanRepo

--- scanRepo.js
+++ scanRepo.js
@@ -1,6 +1,8 @@
-const simpleGit = require('simple-git/promise')
-const scanDirectory = require('./scanDirectory')
+const git = require('isomorphic-git')
+const fs = require('fs')
+git.plugins.set('fs', fs)
 
+/*
 async function getCurrentHead (git) {
   const status = await git.status()
 
@@ -11,37 +13,9 @@ async function getCurrentHead (git) {
     return status.current
   }
 }
+*/
 
 module.exports = async function scanRepo (repoPath, { from, to } = {}) {
-  const git = simpleGit(repoPath)
-
-  if (!(await git.checkIsRepo())) {
-    console.log(`Directory [${repoPath}] is not a git repository`)
-  }
-
-  const status = await git.status()
-
-  if (!status.isClean()) {
-    console.log(`The repo [${repoPath}] is not clean.`)
-  }
-
-  const head = await getCurrentHead(git)
-  const commits = (await git.log({ from, to })).all
-  let vulnerabilities = []
-
-  for (const commit of commits) {
-    await git.checkout(commit.hash)
-    console.group(`Commit ${commit.hash}`)
-    const vuls = (await scanDirectory(repoPath, (p) => git.checkIgnore(p)))
-      .map(v => ({
-        commit: commit.hash,
-        ...v
-      }))
-
-    vulnerabilities = vulnerabilities.concat(vuls)
-    console.groupEnd()
-  }
-
-  await git.checkout(head)
-  return vulnerabilities
+  await git.findRoot({ filepath: repoPath })
+  // const files = await git.listFiles({ dir: repoRoot, ref: 'HEAD' })
 }
--- test/scanRepo.js
+++ test/scanRepo.js
@@ -1,16 +1,9 @@
 const test = require('ava')
+const tmp = require('tmp')
 const scanRepo = require('../scanRepo.js')
-const path = require('path')
 
-test('Detect a token that appears in one commit only', async t => {
-  const vulnerabilities = (await scanRepo(path.resolve(__dirname, 'fake-data/')))
-    .map(v => ({
-      secrets: v.secrets,
-      commit: v.commit
-    }))
-
-  t.deepEqual(vulnerabilities, [{
-    secrets: ['8779~this_is_also_a_token'],
-    commit: '9532a18'
-  }])
+test('Calling "scanRepo" in a non-repo throws', async t => {
+  const d = tmp.dirSync({ prefix: 'lms-scan__test' })
+  await t.throwsAsync(() => scanRepo(d.name))
+  d.removeCallback()
 })
