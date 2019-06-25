b034532ea9d71d9908634b895b1d9025113c4d6e
Carlos Saito <carlos@saito.style>
2019-06-25 15:10:18 +0200

    When "history" with no more args, scan the whole history

--- index.js
+++ index.js
@@ -3,17 +3,31 @@ const scanDirectory = require('./scanDirectory')
 const scanRepo = require('./scanRepo')
 const { format } = require('util')
 
-if (process.argv.length > 3 && process.argv[2] === 'history') {
-  const found = process.argv[3].match(/(\w+)\.\.\.(\w+)/)
-  if (found === null) {
-    console.log('Format not valid')
-    process.exit(1)
+if (process.argv.length >= 3 && process.argv[2] === 'history') {
+  let result
+  if (process.argv.length >= 4) {
+    const found = process.argv[3].match(/(\w+)\.\.\.(\w+)/)
+    if (found === null) {
+      console.log('Format not valid')
+      process.exit(1)
+    }
+    const [, from, to] = found
+    result = scanRepo(process.cwd(), { from, to })
+  } else {
+    result = scanRepo(process.cwd())
   }
 
-  const [, from, to] = found
-  scanRepo(process.cwd(), { from, to })
+  result
     .then((vulnerabilities) => {
-      console.log(vulnerabilities)
+      for (const entry of vulnerabilities) {
+        console.log(format(`[commit: ${entry.commit}]`))
+        console.log(format(`[${entry.filepath}]`))
+
+        for (const secret of entry.secrets) {
+          console.log(`>> ${secret}`)
+        }
+        console.log()
+      }
     })
     .catch(e => {
       if (e.name === 'GitRootNotFoundError') {
