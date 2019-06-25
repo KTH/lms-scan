cd10d2be87cb30d80431638d6d38fc70c2d341df
Carlos Saito <carlos@saito.style>
2019-06-25 13:50:06 +0200

    Add test for checking agains "parallel" commits

--- test/scanRepo.js
+++ test/scanRepo.js
@@ -14,3 +14,12 @@ test('Calling "scanRepo" in a non-repo throws', async t => {
 test('Calling "scanRepo" with non-existing commits SHA throws', async t => {
   await t.throwsAsync(() => scanRepo(fakeDir, { from: 'xxx', to: 'xxx' }))
 })
+
+test('Calling "scanRepo" with non-ancestor commits throws', async t => {
+  await t.throwsAsync(() => scanRepo(fakeDir, { from: '6c4f8df', to: 'e4f270e' }))
+})
+
+test('Happy path', async t => {
+  await scanRepo(fakeDir, { from: '4c56649', to: '17d808f' })
+  t.pass()
+})
