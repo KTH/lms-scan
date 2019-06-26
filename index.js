7b43809667186966710f214dca27402e604f65db
Carlos Saito <carlos@saito.style>
2019-06-26 14:08:46 +0200

    Include check against Canvas

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 const scanDirectory = require('./scanDirectory')
 const scanRepo = require('./scanRepo')
+const Canvas = require('@kth/canvas-api')
 
 function printVulnerabilities (vulnerabilities) {
   for (const entry of vulnerabilities) {
@@ -16,12 +17,37 @@ function printVulnerabilities (vulnerabilities) {
   }
 }
 
+async function checkAgainstCanvas (vulnerabilities) {
+  const invalidTokens = new Map()
+  for (const entry of vulnerabilities) {
+    entry.secrets.forEach(secret => invalidTokens.set(secret, false))
+  }
+
+  for (const [token] of invalidTokens) {
+    const canvas = Canvas('https://kth.instructure.com/api/v1', token)
+    try {
+      await canvas.get('/users/self')
+    } catch (e) {
+      invalidTokens.set(token, e.statusCode === 401)
+    }
+  }
+
+  return vulnerabilities
+    .map(v => ({
+      ...v,
+      secrets: v.secrets.filter(s => !invalidTokens.get(s))
+    }))
+    .filter(v => v.secrets.length > 0)
+}
+
 // eslint-disable-next-line no-unused-expressions
 require('yargs')
-  .usage('Usage: $0')
-  .usage('Usage: $0 history [range]')
   .command('$0', 'Scan the current directory', () => {}, async (argv) => {
-    const found = await scanDirectory(process.cwd())
+    let found = await scanDirectory(process.cwd())
+    if (argv.r) {
+      found = await checkAgainstCanvas(found)
+    }
+
     printVulnerabilities(found)
     process.exit(found.length === 0 ? 0 : 1)
   })
@@ -39,11 +65,18 @@ require('yargs')
     } else {
       found = await scanRepo(process.cwd())
     }
+    if (argv.r) {
+      found = await checkAgainstCanvas(found)
+    }
+
     printVulnerabilities(found)
     process.exit(found.length === 0 ? 0 : 0)
   })
   .example('$0 history', 'Scan through all the git repository history')
   .example('$0 history <start>...<end>', 'Scan the git repository from the "<start>" commit to the "<end>" commit.\n\nNote: The "<start>" commit is not included but "<end>" is')
+  .boolean(['r'])
+  .alias('r', 'remote')
+  .describe('r', 'Check found tokens against Canvas')
   .help('h')
   .alias('h', 'help')
   .demandCommand()
